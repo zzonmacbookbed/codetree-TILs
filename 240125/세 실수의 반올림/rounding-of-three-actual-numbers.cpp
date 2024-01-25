f63af460a4af0a7234fa3aaee92685559627580e
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-25 18:03:17 +0900

    Title: 세 실수의 반올림, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240125/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
+++ 240125/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
-
+using namespace std
 int main() {
     double a,b,c;
     cin >> a >> b >> c;
     cout << fixed;
     cout.precision(3);
     cout << a << endl << b << endl << c;
-    
+
 }

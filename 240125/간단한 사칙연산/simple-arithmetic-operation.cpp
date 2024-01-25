fb8a0dca34580be4fa03a35c7c3a7757faa7a477
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-25 18:44:41 +0900

    Title: 간단한 사칙연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240125/간단한 사칙연산/simple-arithmetic-operation.cpp
+++ 240125/간단한 사칙연산/simple-arithmetic-operation.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() {
     int a,b;
     cin >> a >> b;
-    cout << a+b << "\n" << a-b << "\n" << a*b << "\n" << a%b;
+    cout << a+b << "\n" << a-b << "\n" << a/b << "\n" << a%b;
 
 }

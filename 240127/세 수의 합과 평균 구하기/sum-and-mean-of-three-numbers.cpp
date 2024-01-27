9995f570bebc28c62311c1ca36e509f554173da5
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-27 14:11:15 +0900

    Title: 세 수의 합과 평균 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240127/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    cout << fixed;
+    cout.precision(0);
+    cout << a + b + c << endl << double(a + b + c)/3;
+}

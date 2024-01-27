d75e2091ac81838c9eaf657f61fe46e0f6aea958
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-27 14:32:24 +0900

    Title: 합과 평균의 차, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240127/합과 평균의 차/sub-of-average-and-sum.cpp
+++ 240127/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main() {
     int a,b,c;
     cin >> a >> b >> c;
-    cout << a + b + c << (a + b + c)/3 << (a + b + c) -(a + b + c)/3
+    cout << a + b + c << (a + b + c)/3 << (a + b + c) -(a + b + c)/3;
     return ;
 }

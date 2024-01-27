77eaa81dbb6b9df087bf96e2f4eb9c0f57ac4a35
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-27 15:47:44 +0900

    Title: 알파벳에 따른 평가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240127/알파벳에 따른 평가/evaluation-by-alphabet.cpp
+++ 240127/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int main() {
-    char a;
+    string a;
+    cin >> a;
     a == "S" ? cout << "Superior" : a == "A" ? cout << "Excellent" : a == "B" ? cout << "Good" : a == "C" ? cout << "Usually" : a == "D" ? cout << "Effort" : cout << "Failure";
     return 0;
 }

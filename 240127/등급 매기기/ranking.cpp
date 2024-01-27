a355f1a0dd2da1ae80907df151d51582e5f0d01e
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-27 15:40:15 +0900

    Title: 등급 매기기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240127/등급 매기기/ranking.cpp
+++ 240127/등급 매기기/ranking.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-
+using namespace std
 int main() {
     int a;
     cin >> a;
-    a >= 90 ? cout << "A" : a >= 80 ? cout << "B" : a >= 70 ? cout << "C" : a >= 60 ? cout << "D" : cout << "E";
+    a >= 90 ? cout << "A" : a >= 80 ? cout << "B" : a >= 70 ? cout << "C" : a >= 60 ? cout << "D" : cout << "F";
 }

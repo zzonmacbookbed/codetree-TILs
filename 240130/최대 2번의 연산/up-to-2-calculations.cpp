c639f09711e0f9ac89d7d7400ea3322bc5318ce0
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-30 10:30:49 +0900

    Title: 최대 2번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240130/최대 2번의 연산/up-to-2-calculations.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if (a%2==0) {
+        a /= 2;
+    }
+    if (a%2==1) {
+        a++;
+        a /= 2;
+    }
+
+    cout << a;
+}

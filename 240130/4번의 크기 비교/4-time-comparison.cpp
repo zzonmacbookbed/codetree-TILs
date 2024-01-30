e005832f3a7f71d3328437d021d917c3eeb96f09
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-30 13:07:41 +0900

    Title: 4번의 크기 비교, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240130/4번의 크기 비교/4-time-comparison.cpp
+++ 240130/4번의 크기 비교/4-time-comparison.cpp
@@ -5,6 +5,6 @@ int main() {
     cin >> a >> b >> c >> d >> e;
     cout << (a > b) << endl;
     cout << (a > c) << endl;
-    cout << (b > d) << endl;
-    cout << (b > e) << endl;
+    cout << (a > d) << endl;
+    cout << (a > e) << endl;
 }

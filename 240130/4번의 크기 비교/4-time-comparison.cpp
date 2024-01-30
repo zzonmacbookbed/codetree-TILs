c0a2aeac8e7415631120ebd8a5c664bf990e53ba
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-01-30 13:05:33 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240130/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d,e;
+    cin >> a >> b >> c >> d >> e;
+    cout << (a > b) << endl;
+    cout << (a > c) << endl;
+    cout << (b > d) << endl;
+    cout << (b > e) << endl;
+}

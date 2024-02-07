b2d719a316b3c1f59ef7a307254bd21f470bd39a
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 07:08:34 +0900

    Title: 굉장한 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/굉장한 숫자/amazing-number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    if ((a % 3 == 0 && a % 2 != 0) || (a % 5 == 0 && a % 2 == 0)) {
+        cout << "true";
+        }
+    else {
+        cout << "false";
+        }
+}

bb7970e9fa6d8b9c87af96f885412830b978b214
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 07:16:20 +0900

    Title: 장학금, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240208/장학금/scholarship.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin >> a >> b;
+    if (a > 90 && b >= 95) {
+        cout << 100000;
+    }
+    else if (a > 90 && b >= 90) {
+        cout << 100000/2;
+    }
+    else {
+        cout << 0;
+    }
+}

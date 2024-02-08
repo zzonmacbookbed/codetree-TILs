e7f5900cc0709ce87237725b7958360b8e440ca4
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 18:04:26 +0900

    Title: a/b 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240208/a/b 출력/a-divide-b.cpp
+++ 240208/a/b 출력/a-divide-b.cpp
@@ -1,13 +1,4 @@
 #include <iostream>
-using namespace std;
-int main() {
-    double a,b;
-    cin >> a >> b;
-    cout << fixed;
-    cout.precision(20);
-    cout << a / b;
-    return 0;
-#include <iostream>
 
 using namespace std;
 

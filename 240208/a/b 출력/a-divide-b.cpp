31ce74bab1a68f90383f41d4288f86079b9f40b3
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 18:04:16 +0900

    Title: a/b 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240208/a/b 출력/a-divide-b.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a,b;
+    cin >> a >> b;
+    cout << fixed;
+    cout.precision(20);
+    cout << a / b;
+    return 0;
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언 및 입력
+	int a, b;
+    cin >> a >> b;
+	
+    // 정수 부분을 먼저 출력합니다.
+    cout << a / b << ".";
+    
+    // a를 b로 나눈 나머지를 시작으로
+    // 소수점 자리를 하나씩 계산합니다.
+    a %= b;
+	for(int i = 0; i < 20; i++) {
+        // 나머지에 10 곱한 값을 기준으로
+        // b로 나누었을 떄의 몫을 구해줍니다.
+		a *= 10;
+        cout << a / b;
+        
+        // a를 b로 나눈 나머지를 게속 갱신해줍니다.
+		a %= b;
+	}
+	
+	return 0;
+}

65ea1aec660529cc1fca5eb3802e11e480d1496b
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 18:29:26 +0900

    Title: 숫자의 증감, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240208/숫자의 증감/increasing-and-decreasing-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a;
+    int b;
+    if(a == "A") {
+        for(int i = 1; i <= b; i++) {
+            cout << i << " ";
+        }
+    }
+    else {
+        for(int i = b; i >= 1; i--) {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}

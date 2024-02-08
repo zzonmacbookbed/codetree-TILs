4f42600572b24bf149f6a9ca66a6b1f28a25d7c6
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 10:49:37 +0900

    Title: 좀 더 어려운 수학 점수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240208/좀 더 어려운 수학 점수/math-scores-are-more-difficult.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c,d;
+    cin >> a >> b >> c >> d;
+
+    if (a >= c || c >= a) {
+        if (a >= c) {
+            cout << "A";
+        }
+        else {
+            cout << "B";
+        }
+    }
+    else if (a == c) {
+        if (b > d) {
+            cout << "A";
+        }
+        else {
+            cout << "B";
+        }
+    }
+}

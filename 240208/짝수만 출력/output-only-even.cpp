4b2627002eaefd940b2e70affe7f8530e48bf2eb
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 17:31:34 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240208/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    
+    int a, b, i;
+    
+    cin >> a >> b;
+
+    i = a;
+
+    while (i <= b) {
+
+        cout << i << " ";
+
+        i += 2;
+
+    }
+    return 0;
+}

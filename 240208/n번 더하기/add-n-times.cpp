7c4f8f44c9215d13e20d797b122d5b930ab8515f
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-08 18:09:49 +0900

    Title: n번 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240208/n번 더하기/add-n-times.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,i;
+    cin >> a >> b;
+    i = a + b;
+    for(i; i<= (a*b) + a + b; i += b ) {
+        cout << i << "\n";
+
+    }
+}

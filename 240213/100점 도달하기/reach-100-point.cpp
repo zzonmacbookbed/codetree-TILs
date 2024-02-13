6772ef5579afe397bc13db05e1aec20f3454d262
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-13 18:09:33 +0900

    Title: 100점 도달하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240213/100점 도달하기/reach-100-point.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    for(int i = a; i <= 100; i++) {
+        if (i >= 90) {
+            cout << "A ";
+        }
+        else if (i >= 80) {
+            cout << "B ";
+        }
+        else if (i >= 70) {
+            cout << "C ";
+        }
+        else if (i >= 60) {
+            cout << "D ";
+        }
+        else {
+            cout << "F ";
+        }
+    }
+}

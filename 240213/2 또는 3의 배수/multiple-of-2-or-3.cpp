3795b37a2c8092349a57ecfe627dc51a681ee27c
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-13 17:54:17 +0900

    Title: 2 또는 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240213/2 또는 3의 배수/multiple-of-2-or-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    for(int i = 1; i <= a; i++) {
+        if (i%2==0 || i%3==0) {
+            cout << 1 << " ";
+        }
+        else {
+            cout << 0 << " ";
+        }
+    }
+}

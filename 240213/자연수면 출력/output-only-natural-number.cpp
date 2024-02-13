25bb1027abf5329cb37e3629b8bcffb5b60f5ad8
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-13 17:34:26 +0900

    Title: 자연수면 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240213/자연수면 출력/output-only-natural-number.cpp
+++ 240213/자연수면 출력/output-only-natural-number.cpp
@@ -3,12 +3,12 @@ using namespace std;
 int main() {
     int a,b;
     cin >> a >> b;
-    if (a < 0) {
+    if (a <= 0) {
         cout << 0;
     }
     else {
         for(int i = 1; i<=b; i++) {
         cout << a;
-    }
+        }
     }
 }

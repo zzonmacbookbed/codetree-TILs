306213640f5afe1c9a0449591811ccd36f0af93f
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-15 05:01:08 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240215/369 게임/369-game.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    cin >> a;
+    for(int i = 1; i <= a; i++) {
+        if(i%3==0) {
+            cout << 0 << " ";
+        }
+        else {
+            cout << i << " ";
+        }
+    }
+
+}

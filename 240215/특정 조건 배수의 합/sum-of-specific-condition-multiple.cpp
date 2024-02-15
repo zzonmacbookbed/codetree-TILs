828627a0d9f7a317b1ccbc16c9b2fcb3294ae51c
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-15 18:28:30 +0900

    Title: 특정 조건 배수의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
+++ 240215/특정 조건 배수의 합/sum-of-specific-condition-multiple.cpp
@@ -4,9 +4,18 @@ int main() {
     int a,b;
     cin >> a >> b;
     int sum = 0;
-    for(int i = a; i <= b; i++) {
-        if(i%5==0) {
+    if(b < a) {
+        for(int i = b; i <= a; i++) {
+           if(i%5==0) {
             sum += i;
+            } 
+        }
+    }
+    else {
+        for(int i = a; i <= b; i++) {
+            if(i%5==0) {
+            sum += i;
+            }
         }
     }
     cout << sum;

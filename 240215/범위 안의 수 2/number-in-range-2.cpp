2c8f0b44259a20271a90e52e2db00c5096d72d7f
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-15 18:38:15 +0900

    Title: 범위 안의 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/범위 안의 수 2/number-in-range-2.cpp
+++ 240215/범위 안의 수 2/number-in-range-2.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 int main() {
     int a, sum = 0;
+    int cnt = 0;
     for(int i = 0; i < 10; i++) {
         cin >> a;
         if(a >= 0 && a <= 200) {
             sum += a;
+            cnt += 1;
         
         }
 
     }
     cout << fixed;
     cout.precision(1);
-    cout << sum << " " << (double)sum/10;
+    cout << sum << " " << (double)sum/cnt;
     return 0;
 }

c81905d9e35d8a805ba1e2647b840e17b0c50f6a
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-15 17:55:09 +0900

    Title: a부터 b까지의 합, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240215/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    int sum = 0;
+    cin >> a >> b;
+    for(int i = a; i <= b; i++) {
+        sum += i;
+    }
+    cout << sum;
+    return 0;
+}

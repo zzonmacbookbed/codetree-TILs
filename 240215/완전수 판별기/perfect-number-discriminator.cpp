06b623eff92e7f4c2304aed7588ed5951ed93aae
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-15 18:47:40 +0900

    Title: 완전수 판별기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240215/완전수 판별기/perfect-number-discriminator.cpp
+++ 240215/완전수 판별기/perfect-number-discriminator.cpp
@@ -3,7 +3,8 @@ using namespace std;
 int main() {
     int a;
     int sum = 0;
-    for(int i = 1; i<= a; i++) {
+    cin >> a;
+    for(int i = 1; i < a; i++) {
         if(a%i == 0) {
             sum += i;
         }

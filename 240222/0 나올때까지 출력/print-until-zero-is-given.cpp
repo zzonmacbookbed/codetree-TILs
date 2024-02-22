82b35aea8fb2610705bd6152e4536a0baf62309f
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-22 18:17:25 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240222/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    while (true) {
+        cin >> n;
+        if(n == 0) {
+            break;
+        }
+        cout << n << endl;
+    }
+    return 0;
+}

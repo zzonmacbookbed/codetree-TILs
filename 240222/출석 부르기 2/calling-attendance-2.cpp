b68087e49421f84e8808a27aeb2708d1fff91b45
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-02-22 18:29:57 +0900

    Title: 출석 부르기 2, Time: 1032ms, Memory: 80MB, Status: Time Limit Exceed - CodeTree

---
+++ 240222/출석 부르기 2/calling-attendance-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    while (true) {
+        cin >> a;
+        if(a==1){
+            cout << "John" << endl;
+        }
+        if(a==2) {
+            cout << "Tom" << endl;
+        }
+        if(a==3) {
+            cout << "Paul" << endl;
+        }
+        if(a==4) {
+            cout << "Sam" << endl;
+        }
+        else {
+            cout << "Vacancy" ;
+        }
+    }
+    return 0;
+}

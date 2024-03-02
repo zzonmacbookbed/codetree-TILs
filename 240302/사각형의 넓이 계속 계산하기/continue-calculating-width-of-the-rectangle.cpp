7802d80f76d459d0229eac50433b210d791444e7
zzonmacbookbed <113758657+zzonmacbookbed@users.noreply.github.com>
2024-03-02 14:58:45 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240302/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    string x;
+    while(true) {
+        cin >> a >> b >> x;
+        if(x=="C") {
+            cout << a*b << endl;
+            break;
+        }
+        else {
+            cout << a*b << endl;
+        }
+    }
+
+
+
+}
